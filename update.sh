e03d63c2cb1a222539ba25e9d01c24f70d5af799
Jan Sarenik <jajomojo@gmail.com>
2023-12-08 09:14:04 +0000

    update realtime_btc

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 rm realtime_btc
+rm api/time-to-halving
 wget https://nakamotoportfolio.com/realtime_btc
+cd api
+wget https://nakamotoportfolio.com/api/time-to-halving
+cd -
 git commit -a -m "update realtime_btc"
+git push
